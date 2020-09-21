3b2a4a5969bf22909665eff02d6f0d3cf95e4540
Nova <thbb268@live.com>
2020-09-22 00:26:28 +0800

    map pojo

---
+++ src/main/java/cn/th/mhgu/pojo/Map.java
@@ -0,0 +1,42 @@
+package cn.th.mhgu.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "map")
+public class Map {
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "map_name")
+    private String mapName;
+    @Column(name = "pic_name")
+    private String picName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMapName() {
+        return mapName;
+    }
+
+    public void setMapName(String mapName) {
+        this.mapName = mapName;
+    }
+
+    public String getPicName() {
+        return picName;
+    }
+
+    public void setPicName(String picName) {
+        this.picName = picName;
+    }
+}
